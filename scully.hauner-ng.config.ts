bd6ecd74664b34ed12a648c87373ed9ff0e48cda
Martin Hauner <martin.hauner@gmx.net>
2020-05-15 22:52:08 +0200

    add scully

---
+++ scully.hauner-ng.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "hauner-ng",
+  outDir: './dist/static',
+  routes: {
+  }
+};
