1abcdbb877e072ea651374e1b8e40418d8e4bf68
Martin Hauner <martin.hauner@gmx.net>
2020-05-16 10:44:36 +0200

    header & menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable} from "rxjs";
+import {Component, Inject, OnInit, Renderer2} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
 import {ScullyRoutesService} from "@scullyio/ng-lib";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,15 @@ export class HomeComponent implements OnInit {
 
   links$: Observable<any> = this.scully.available$;
 
-  constructor(private scully: ScullyRoutesService) {}
+  menuVisible = false;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+    private scully: ScullyRoutesService
+  ) {
+
+  }
 
   ngOnInit() {
     this.links$.subscribe(links => {
@@ -19,5 +28,23 @@ export class HomeComponent implements OnInit {
     });
   }
 
+
+  showMenu (event: Event) {
+    console.log ('showMenu()');
+    this.menuVisible = true;
+    this.renderer.addClass(this.document.body, 'is-menu-visible');
+
+    // no click on parents
+    event.stopPropagation();
+  }
+
+  hideMenu () {
+    console.log ('hideMenu()');
+
+    if (this.menuVisible) {
+      this.renderer.removeClass(this.document.body, 'is-menu-visible');
+      this.menuVisible = false;
+    }
+  }
 
 }
