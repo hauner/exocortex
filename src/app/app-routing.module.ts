fe46be82579e8b03f1d7619bce775a5f8762defc
Martin Hauner <martin.hauner@gmx.net>
2020-05-16 07:56:34 +0200

    added blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{
-  path: '',
-  loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-}, {
-  path: 'home',
-  loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-}];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  }, {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  }, {
+    path: 'blog',
+    loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
