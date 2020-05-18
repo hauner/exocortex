257b80bb4e870101cfc33d547417bfdf39af70f6
Martin Hauner <martin.hauner@gmx.net>
2020-05-18 15:41:24 +0200

    blog component

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,19 +1,71 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {ActivatedRoute, Router, ROUTES} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Location} from '@angular/common';
+import {ScullyRoute, ScullyRoutesService} from "@scullyio/ng-lib";
+import {from, Subscription} from "rxjs";
+import {flatMap, map, pairwise, switchMap, tap} from 'rxjs/operators';
 
-declare var ng: any;
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss'],
-  preserveWhitespaces: true,
-  encapsulation: ViewEncapsulation.Emulated
-
+  preserveWhitespaces: true
 })
 export class BlogComponent implements OnInit {
-  ngOnInit() {}
 
-  constructor(private router: Router, private route: ActivatedRoute) {
+  sub: Subscription;
+
+  current: ScullyRoute;
+  routePrev: ScullyRoute;
+  routeNext: ScullyRoute;
+
+  constructor(private router: Router, private route: ActivatedRoute, private scully: ScullyRoutesService, private location: Location) {
   }
+
+  ngOnInit() {
+    this.location.onUrlChange((url) => {
+      this.initCurrent();
+    });
+
+    this.initCurrent();
+  }
+
+  private initCurrent () {
+    if (this.sub != null) {
+      this.sub.unsubscribe();
+    }
+
+    this.sub = this.scully.getCurrent ()
+      .pipe(
+        tap(r => {
+          this.current = r;
+          this.routePrev = null;
+          this.routeNext = null;
+        }),
+        switchMap(() => this.scully.available$
+            .pipe(
+              map (routes => {
+                return routes
+                  .filter ((r) => {
+                    return (r.published != null) && (r.published === true);
+                  })
+                  .sort((l, r) => {
+                    return l.date.localeCompare (r.date);
+                  })
+              }),
+              flatMap(from),
+              pairwise()
+            )
+        )
+      )
+      .subscribe((routes: ScullyRoute[]) => {
+          if (routes[0].route == this.current.route) {
+            this.routeNext = routes[1];
+          } else if (routes[1].route == this.current.route) {
+            this.routePrev = routes[0];
+          }
+      })
+  }
+
 }
--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,9 +3,15 @@ import {NgModule} from '@angular/core';
 import {ScullyLibModule} from '@scullyio/ng-lib';
 import {BlogRoutingModule} from './blog-routing.module';
 import {BlogComponent} from './blog.component';
+import {PartsModule} from "../parts/parts.module";
 
 @NgModule({
   declarations: [BlogComponent],
-  imports: [CommonModule, BlogRoutingModule, ScullyLibModule],
+  imports: [
+    CommonModule,
+    BlogRoutingModule,
+    ScullyLibModule,
+    PartsModule
+  ]
 })
 export class BlogModule {}
