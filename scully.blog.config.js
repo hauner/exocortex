6aa12695fc444309769bc7cd2c04cbabd39931f4
Martin Hauner <martin.hauner@gmx.net>
2020-05-19 15:42:50 +0200

    build to docs

--- scully.blog.config.js
+++ scully.blog.config.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = {
     projectRoot: "./src",
     projectName: "blog",
-    outDir: './dist/static',
+    outDir: './docs',
     routes: {
         '/blog/:slug': {
             type: 'contentFolder',
--- scully.blog.config.ts
+++ scully.blog.config.ts
@@ -3,7 +3,7 @@ import {ScullyConfig} from '@scullyio/scully';
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "blog",
-  outDir: './dist/static',
+  outDir: './docs',
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
