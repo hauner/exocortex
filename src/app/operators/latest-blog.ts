1dd4d64066014fd6f9d89e19d9a6b5e7fa0908f3
Martin Hauner <martin.hauner@gmx.net>
2020-05-19 08:22:11 +0200

    get all published articles

--- src/app/operators/latest-blog.ts
+++ src/app/operators/latest-blog.ts
@@ -15,3 +15,22 @@ export function latestBlog (): OperatorFunction<ScullyRoute[], ScullyRoute> {
       )
   };
 }
+
+export function latestBlogs (count?: number): OperatorFunction<ScullyRoute[], ScullyRoute[]> {
+  return (source$: Observable<ScullyRoute[]>) => {
+    return source$
+      .pipe(
+        map (routes => {
+          let result = routes
+            .filter (r => r.published != null && r.published)
+            .sort((l, r) => r.date.localeCompare (l.date));
+
+            if (count != null) {
+              result = result.slice(0, count)
+            }
+
+            return result;
+        })
+      )
+  };
+}
