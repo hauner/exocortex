95890312050553c6ed3640289fc5e006a6a9d4b0
Martin Hauner <martin.hauner@gmx.net>
2020-05-19 08:21:34 +0200

    enable/disable sidebar article list

--- src/app/parts/sidebar/sidebar.component.ts
+++ src/app/parts/sidebar/sidebar.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {ScullyRoute, ScullyRoutesService} from "@scullyio/ng-lib";
-import {map} from "rxjs/operators";
+import {latestBlogs} from "../../operators/latest-blog";
 
 @Component({
   selector: 'app-sidebar',
@@ -12,13 +12,11 @@ export class SidebarComponent implements OnInit {
 
   links$: Observable<ScullyRoute[]> = this.scully.available$
     .pipe(
-      map( (r: ScullyRoute[]) => {
-          return r
-            .filter(i => (i.published != null) && (i.published === true))
-            .sort((l, r) => l.date.localeCompare(r.date))
-            .slice(0, 5);
-      })
+      latestBlogs ()
     )
+
+  @Input()
+  showArticles = false;
 
   constructor(private scully: ScullyRoutesService) { }
 
