b2ac6dddfa80a4dc50cc8d45ed700d64ca289bc8
Martin Hauner <martin.hauner@gmx.net>
2020-05-23 10:44:04 +0200

    full site build

--- exocortex.sh
+++ exocortex.sh
@@ -1,4 +1,14 @@
+set -x
+
+ng build --prod --output-hashing none
+npm run scully -- --scanRoutes --showGuessError --removeStaticDist
+
+rm -r -v docs/*
+
+cp -R dist/static/ docs
+
 find docs -type f -name "index.html" -exec sed -i '' -e 's:<base href="/">:<base href="/exocortex/">:' {} \;
 find docs -type f -name "404.html" -exec sed -i '' -e 's:<base href="/">:<base href="/exocortex/">:' {} \;
 find docs -type f -name "*.css" -exec  sed -i '' -e 's:/assets/:/exocortex/assets/:g' {} \;
 find docs -type f -name "main-*.js" -exec  sed -i '' -e 's:/assets/scully-routes.json:/exocortex/assets/scully-routes.json:g' {} \;
+
