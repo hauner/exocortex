01830602dd3a4625628174bc27e488c73643c0d1
Martin Hauner <martin.hauner@gmx.net>
2020-05-23 10:24:53 +0200

    renamed repository

--- github-pages.sh
+++ github-pages.sh
@@ -1,4 +1,4 @@
-find docs -type f -name "index.html" -exec sed -i '' -e 's:<base href="/">:<base href="/test-blog/">:' {} \;
-find docs -type f -name "404.html" -exec sed -i '' -e 's:<base href="/">:<base href="/test-blog/">:' {} \;
-find docs -type f -name "*.css" -exec  sed -i '' -e 's:/assets/:/test-blog/assets/:g' {} \;
-find docs -type f -name "main-*.js" -exec  sed -i '' -e 's:/assets/scully-routes.json:/test-blog/assets/scully-routes.json:g' {} \;
+find docs -type f -name "index.html" -exec sed -i '' -e 's:<base href="/">:<base href="/blog/">:' {} \;
+find docs -type f -name "404.html" -exec sed -i '' -e 's:<base href="/">:<base href="/blog/">:' {} \;
+find docs -type f -name "*.css" -exec  sed -i '' -e 's:/assets/:/blog/assets/:g' {} \;
+find docs -type f -name "main-*.js" -exec  sed -i '' -e 's:/assets/scully-routes.json:/blog/assets/scully-routes.json:g' {} \;
