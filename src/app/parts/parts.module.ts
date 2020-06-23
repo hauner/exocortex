e9f95221d2d34f1a1782a6d4beeedff6615c88f7
Martin Hauner <martin.hauner@gmx.net>
2020-06-23 16:11:51 +0200

    use sidebar component

--- src/app/parts/parts.module.ts
+++ src/app/parts/parts.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {HeaderComponent} from "./header/header.component";
@@ -15,6 +15,7 @@ import {RouterModule} from "@angular/router";
     CommonModule,
     RouterModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [
     HeaderComponent,
     SidebarComponent
